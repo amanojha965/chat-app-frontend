71185f0865b13b581abed603702bccebbfe5e3b6
amanojha965 <amanojha9650@gmail.com>
2026-04-15 14:28:23 +0530

    updated pages && components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,52 @@
-import React, { useEffect } from 'react'
-import Navbar from './components/Navbar'
-import { Routes, Route } from "react-router-dom"
+import Navbar from "./components/Navbar";
 
-import HomePage from './pages/HomePage'
-import ProfilePage from './pages/ProfilePage'
-import LoginPage from './pages/LoginPage'
-import SignUpPage from './pages/SignUpPage'
-import SettingsPage from './pages/SettingsPage'
-import axios from 'axios'
-import { useAuthStore } from './store/useAuthStore'
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
+import { useEffect } from "react";
+
+import { Loader } from "lucide-react";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
-  // const { authUser, checkAuth } = useAuthStore;
-  // useEffect(() => {
-  //   checkAuth()
+  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
+  const { theme } = useThemeStore();
 
-  // }, [checkAuth]);
+  console.log({ onlineUsers });
 
-  // console.log({ authUser });
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({ authUser });
+
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
+
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
 
+      <Toaster />
     </div>
-  )
-}
-
-export default App
+  );
+};
+export default App;
